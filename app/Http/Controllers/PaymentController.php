07d30e3fe137754c48a21a0c7235474dfe3cac3e
Omid <o.shabani@hotmail.com>
2018-06-12 22:49:12 +0430

    /

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,7 +13,6 @@ class PaymentController extends Controller
 
     public function create(Request $request)
     {
-        return 1;
         $total_price = 0;
         $discount = 0;
          $cart_items = session()->get('cart.item');
