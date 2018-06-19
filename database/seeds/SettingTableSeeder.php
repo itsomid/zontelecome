b35caf1631629b5a30964da3fa7742c98b0c08a6
Omid <o.shabani@hotmail.com>
2018-06-19 13:44:26 +0430

    .

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -11,12 +11,24 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
-       $setting = [
-           'tax_fee'     => 5.5,
-           'delivery_fee'    => 2.00,
-           'pay_method' => 0,
-           'created_at' => Carbon::now()->format('Y-m-d H:i:s')
-       ];
+        if (config('app.locale') == 'en')
+        {
+            $setting = [
+                'tax_fee'     => 5.5,
+                'delivery_fee'    => 2.00,
+                'pay_method' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ];
+        }
+        else{
+            $setting = [
+                'tax_fee'     => 0,
+                'delivery_fee'    => 15000,
+                'pay_method' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ];
+        }
+
         DB::table('setting')->insert($setting);
 
     }
