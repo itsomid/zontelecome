c01b361d37e0b8dd7ec625729e573a0de1133aed
Omid <o.shabani@hotmail.com>
2018-06-29 13:07:47 +0430

    sd

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -151,7 +151,7 @@ class PaymentController extends Controller
     {
 
         $order = Order::whereId(Order::realId($order_uid))->first();
-       return $payment = $order->payment->id;
+       return $payment = $order->payment()->get();
         //$payment = Payment::where('id', Payment::realId($payment_uid))->first();
         $checkout_id = $request->input('checkoutId');
         $order_uid = $request->input('referenceId');
