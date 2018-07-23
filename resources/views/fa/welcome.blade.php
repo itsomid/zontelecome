159db82d78a8f8b96b781994bb28b7dc569a9bf0
Omid <o.shabani@hotmail.com>
2018-07-23 16:02:27 +0430

    sdf

--- resources/views/fa/welcome.blade.php
+++ resources/views/fa/welcome.blade.php
@@ -21,7 +21,7 @@
                                     برای نیاز های جهانی شما
                                 @endif
                             </p>
-                            <p class="price">$ {{number_format($product->price,2)}}</p>
+                            <p class="product_price">$ {{number_format($product->price,2)}}</p>
                             {{--<p class="product_price">100,000 تومان</p>--}}
                             <div class="product_description">
 
