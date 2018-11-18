2eb30cd6c944cb290757bffa48cded35ba397d68
Omid <o.shabani@hotmail.com>
2018-11-18 15:19:53 +0330

    asd

--- database/seeds/OrderTableSeeder.php
+++ database/seeds/OrderTableSeeder.php
@@ -566,7 +566,7 @@ class OrderTableSeeder extends Seeder
                 ],
                 [
                     'quantity' => 1,
-                    'product_id' => '4',
+                    'product_id' => '6',
                     'order_id' => '8',
                     'device_id' => 'KF022496',
                     'description' => 'خرید دیتا',
